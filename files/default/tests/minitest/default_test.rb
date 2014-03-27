90ec1961068092ffa905bf045169b25fa2ffc235
Jeff Rak <jrak@10thmagnitude.com>
2014-03-27 15:01:59 -0500

    Add initial tests

--- files/default/tests/minitest/default_test.rb
+++ files/default/tests/minitest/default_test.rb
@@ -1,11 +1,45 @@
 require File.expand_path('../support/helpers', __FILE__)
+include Helpers::Zswebserver
+describe_recipe 'zsWebServer::default' do
 
-describe 'zsWebServer::default' do
+  describe 'IIS test case' do
+ 
+	  it 'IIS isas a service' do 
+	  	service('w3svc').must_be_running
+  	  end
 
-  include Helpers::Zswebserver
+  	  it 'Are ALL App Pools exist and are running?' do
+  	  	assert false, 'test not yet implemented'
+  	  end
 
-  # Example spec tests can be found at http://git.io/Fahwsw
-  it 'runs no tests by default' do
+  	  it 'has redirection config setup appropriate per-environment (STG/PROD)' do
+  	  	assert false, 'test not yet implemented'
+  	  end
+
+
+
+
   end
+
+  describe 'Infrstructure test cases' do
+
+  	it 'installs .NET 2.0' do
+  		directory('%windir%\microsoft.net\framework\2.0.50727').must_exist 
+  	end
+
+  	it 'installs .NET 4.0' do
+  		directory('%windir%\microsoft.net\framework\4.0.30319').must.exist
+  	end
+
+  	it 'is in the correct OU' do
+  	  	assert false, 'test not yet implemented'
+  	end
+
+
+
+
+  end 
+  	
+
 
 end
--- files/default/tests/minitest/support/helpers.rb
+++ files/default/tests/minitest/support/helpers.rb
@@ -3,5 +3,10 @@ module Helpers
     include MiniTest::Chef::Assertions
     include MiniTest::Chef::Context
     include MiniTest::Chef::Resources
+    require 'minitest/spec'
+    require 'chef'
+    require 'minitest/autorun'
+    require 'win32/registry'
+    require 'ruby-wmi'
   end
 end
